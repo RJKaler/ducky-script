d1bc0522212754a6e7e97641cc2a434eb1a79a2c
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2026-03-12 23:32:52 -0400

    Refactor download script and improve error handling

--- ducky-download.sh
+++ ducky-download.sh
@@ -1,43 +1,54 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-#NOTE - THIS IS A TEMPLATE. YOU HAVE TO MANUALLY EXTRACT THE URL FOR THE DOWNLOAD. IF YOU DON'T KNOW HOW TO DO THAT, WELL, SORRY... 
 #shellcheck disable=all
 
-#NOTE: This is done with the assumption that the owners of Hak5 will not issue a takedown request for a copyright infringment
-#Run at your own risk. I do not endorse any unethical behavior regarding the actual physical manual. This is for a digital PDF and I don't think it's even sold in this form
+#NOTE: This is done with the assumption that the owners of Hak5 will not issue a takedown request for a copyright infringment 
+#Run at your own risk. I do not endorse any unethical behavior regarding the actual physical manual. This is for a digital PDF and I don't think it's even sold in this form 
 
 #You can manually download a free copy here: https://shop.hak5.org/products/usb-rubber-ducky-e-book?srsltid=AfmBOoqYed-cFYrUBBfwFhIuVfM_7KLXx0fB6xCCaSbctgYuGHiQwl0w
 
-#Tested on Kubuntu using Ubuntu kernel. Version:
+#Tested on Kubuntu using Ubuntu kernel. Version: 
 # 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
 
 
 
 #   curl -L -O "$filesource"  || echo 'error'
 
-while read -rep "Enter destination dir for download: " dir_target
+while read -rep "Enter destination dir for download: " dir_target 
 do
-  if [[ -d "$dir_target" ]]; then
+  if [[ -d "$dir_target" ]]; then 
     pushd "$dir_target" &>/dev/null &&
     break
-  else
-    echo "Dir does not exist! Try again..."
+  else 
+    echo "Dir does not exist! Try again..." 
   fi
 done
 
-echo "Currently in $PWD"
+if [[ "$PWD" == "$dir_target" ]]; then 
+echo "in correct dir..." 
+else 
+echo "error" 
+exit 1 
+fi
 
-filesource="https://libgen.la/get.php?md5=8638619c05f6042d1b70b3658a2dba7b&key=NHQYGDAJLBM61S9F"
-until curl -fL -O -C - --retry 50 --retry-delay 5 --retry-all-errors "$filesource"
+while :
 do
-        sleep 8
-        echo "waiting for download..."
+ filesource="https://libgen.la/get.php?md5=8638619c05f6042d1b70b3658a2dba7b&key=WYLTVT5U83ADZXHV"
+ if curl -fL -O -C - --retry 50 --retry-delay 5 --retry-all-errors "$filesource"; then
+        echo "Download complete!"
+        break
+    else
+        echo "Download failed, retrying in 5s..."
+        sleep 5
+    fi
 done
 
 rename_file() {
   mv -v get.php Rubber_Ducky.pdf
 }
 
-rename_file
+rename_file 
 
-xdg-open "$dir_target"/Rubber_Ducky.pdf
+xdg-open Rubber_Ducky.pdf
+
+
